4cd96f9826bb917f31213a957dddaedc7c9c1002
shumph10 <sarahhumphrey2016@outlook.com>
2022-08-10 13:36:38 -0400

    adding base and results html

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -19,7 +19,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
