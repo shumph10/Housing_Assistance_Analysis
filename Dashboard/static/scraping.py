2e9414700ae7661c58678d524d02bda804420398
shumph10 <sarahhumphrey2016@outlook.com>
2022-08-09 21:37:06 -0400

    adding resources for results of machine learning model

--- Dashboard/static/scraping.py
+++ Dashboard/static/scraping.py
@@ -2,15 +2,31 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
-from static.config import MONGO_URL
+import random
+import requests
+from config import MONGO_URL
+
 
 #set up flask
 app = Flask(__name__)
 
 #Use pyMondo to set up mongo connection
-app.config["MONGO_URL"] = MONGO_URL
-mongo = PyMongo(app)
+# app.config["MONGO_URL"] = MONGO_URL
+# mongo = PyMongo(app)
 
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/data')
+def refresh():
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
 
 
 
