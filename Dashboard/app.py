a53573d23fd6f6c25256e74b5643e916752aa624
shumph10 <sarahhumphrey2016@outlook.com>
2022-08-10 17:17:16 -0400

    added main nav bar btwn all pages and info on preprocessing for results page

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -17,14 +17,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 @app.route('/results')
 def results():
     return render_template('results.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=8001)
 
 
 
